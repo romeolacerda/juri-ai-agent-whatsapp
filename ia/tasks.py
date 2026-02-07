7e4680766ea5a9f0df6bdef98bd6d65d69211c8c
Romeo <romeolacerdafarias@gmail.com>
2026-02-07 20:23:07 -0300

    first commit

---
+++ ia/tasks.py
@@ -0,0 +1,27 @@
+from ia.agents import JuriAI
+from usuarios.models import Documentos
+from django.shortcuts import get_object_or_404
+
+def ocr_and_markdown_file(instance_id):
+    from docling.document_converter import DocumentConverter
+
+    documentos = get_object_or_404(Documentos, id=instance_id)
+
+    converter = DocumentConverter()
+    result = converter.convert(documentos.arquivo.path)
+    doc = result.document
+    texto = doc.export_to_markdown()
+
+    documentos.content = texto
+    documentos.save()
+
+def rag_documentos(instance_id):
+    documentos = get_object_or_404(Documentos, id=instance_id)
+    JuriAI.knowledge.insert(
+        name=documentos.arquivo.name,
+        text_content=documentos.content,
+        metadata={
+            "cliente_id": documentos.cliente.id,
+            "name": documentos.arquivo.name,
+        },
+    )
---
+++ usuarios/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+from django.contrib.auth.models import User
+from martor.models import MartorField
+
+class Cliente(models.Model):
+    TIPO_CHOICES = [
+        ('PF', 'Pessoa Física'),
+        ('PJ', 'Pessoa Jurídica'),
+    ]
+    nome = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
+    tipo = models.CharField(max_length=2, choices=TIPO_CHOICES, default='PF')
+    status = models.BooleanField(default=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nome
+
+class Documentos(models.Model):
+    TIPO_CHOICES = [
+        ('C', 'Contrato'),
+        ('P', 'Petição'),
+        ('CONT', 'Contestação'),
+        ('R', 'Recursos'),
+        ('O', 'Outro'),
+    ]
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    tipo = models.CharField(max_length=255, choices=TIPO_CHOICES, default='O')
+    arquivo = models.FileField(upload_to='documentos/')
+    data_upload = models.DateTimeField()
+    content = MartorField()
+
+    def __str__(self):
+        return self.tipo
